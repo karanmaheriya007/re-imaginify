e5018b3f7bcdbe028147a1c3317fd354e1f0863a
karanmaheriya007 <karanmaheriya070@gmail.com>
2024-09-11 15:49:50 +0530

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      TransformationsPage
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3000/transformations
+//localhost:3000/transformations/[id] => we will use Dynamic Routes (https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes)
